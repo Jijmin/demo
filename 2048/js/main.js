6b0bb0d3b27e9258029b1d26f409a65af1c61622
Jijmin <503004784@qq.com>
2016-10-23 16:25:10 +0800

    游戏逻辑

--- 2048/js/main.js
+++ 2048/js/main.js
@@ -25,4 +25,48 @@ function init(){
       girdCell.css('left',getPosLeft(i,j));
     }
   }
+  //将board数组转换成一个二维数组
+  for(var i=0;i<4;i++){
+    board[i]=new Array();
+    //初始化每一个board的值
+    for(var j=0;j<4;j++){
+      board[i][j]=0;
+    }
+  }
+  //调用board的更新函数
+  updateBoardView();
+}
+function updateBoardView(){
+  //假如当前的游戏中有了number-cell
+  $('.number-cell').remove();
+  //设定相应的board的值
+  for(var i=0;i<4;i++){
+    for(j=0;j<4;j++){
+      //对每一个board元素生成number-cell
+      $('#grid-container').append('<div class="number-cell" id="number-cell-'+i+'-'+j+'"></div>')
+      var theNumberCell= $('number-cell-'+i+'-'+j);
+      //number-cell的表现是不一样的
+      if(board[i][j]===0){
+        //不显示
+        theNumberCell.css({
+          width:'0px',
+          height:'0px',
+          top:getPosTop(i,j)+50,
+          left:getPosLeft(i,j)+50
+        });
+      }else{
+        theNumberCell.css({
+          width:'100px',
+          height:'100px',
+          top:getPosTop(i,j),
+          left:getPosLeft(i,j),
+          //根据数字的不同，背景颜色相应改变
+          backgroundColor:getNumberBackgroundColor(board[i][j]),
+          color:getNumberColor(board[i][j])
+        });
+        //显示数组的值
+        theNumberCell.text(board[i][j]);
+      }
+    }
+  }
 }
--- 2048/js/support.js
+++ 2048/js/support.js
@@ -1,9 +1,37 @@
 /**
  * Created by Administrator on 2016/10/23.
  */
+//每一行距离顶部的计算
 function getPosTop(i,j){
   return 20+i*120;
 }
+//每一列距离左边的计算
 function getPosLeft(i,j){
   return 20+j*120;
+}
+//每个数字对应不同颜色赋值
+function getNumberBackgroundColor(number){
+  switch(number){
+    case 2:return '#eee4de';break;
+    case 4:return '#ede0c8';break;
+    case 8:return '#f2b179';break;
+    case 16:return '#f59563';break;
+    case 32:return '#f67c5f';break;
+    case 64:return '#f65e3b';break;
+    case 128:return '#edcf72';break;
+    case 256:return '#edcc61';break;
+    case 512:return '#99cc00';break;
+    case 1024:return '#33b5e5';break;
+    case 2048:return '#0099cc';break;
+    case 4096:return '#aa66cc';break;
+    case 8192:return '#9933cc';break;
+  }
+  return 'black';
+}
+//数字颜色
+function getNumberColor(number){
+  if(number<=4){
+    return '#ff6e65';
+  }
+  return 'white';
 }
