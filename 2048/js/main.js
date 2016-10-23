9e7bd286148df7c894db839d46d8851ea2e2f8a3
Jijmin <503004784@qq.com>
2016-10-23 15:26:57 +0800

    构建页面元素

--- 2048/js/main.js
+++ 2048/js/main.js
@@ -1,3 +1,28 @@
 /**
  * Created by Administrator on 2016/10/23.
  */
+var board=new Array();//游戏主要数据
+var score=0;//分数
+$(function(){
+  //开始一个新的游戏
+  newgame();
+});
+//新的游戏
+function newgame(){
+  //初始化棋盘格
+  init();
+  //在随机的两个格子中生成数字
+}
+//初始化棋盘格
+function init(){
+  //双重循环对这16个格子的位置进行赋值
+  for(var i=0;i<4;i++){//控制行
+    for(j=0;j<4;j++){//控制列
+      //获取到小格子
+      var girdCell = $('#grid-cell-'+i+'-'+j);
+      //设置小格子的top和left值
+      girdCell.css('top',getPosTop(i,j));
+      girdCell.css('left',getPosLeft(i,j));
+    }
+  }
+}
--- 2048/js/support.js
+++ 2048/js/support.js
@@ -1,3 +1,9 @@
 /**
  * Created by Administrator on 2016/10/23.
  */
+function getPosTop(i,j){
+  return 20+i*120;
+}
+function getPosLeft(i,j){
+  return 20+j*120;
+}
