fe3535fe81273268eb72a5f2622552d6130ef7e3
Jijmin <503004784@qq.com>
2017-11-01 22:26:15 +0800

    移除音乐播放器

--- react-music/README.md
+++
@@ -1,3 +0,0 @@
-### 效果图
-![播放页](./static/images/主页.png)
-![列表页](./static/images/列表页.png)
--- react-music/app/utils/util.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by v_yinggzhou on 2017/11/1.
- */
-export function randomRange(under, over) {
-    return Math.ceil(Math.random() * (over - under) + under);
-}
